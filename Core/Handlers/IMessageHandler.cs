e19a8f6fb12b53f32e17f0961438819c1a11539e
Дима <dima.matvienko239@gmail.com>
2025-12-27 06:44:42 +0200

    feat(handlers): add IMessageHandler abstraction

---
+++ Core/Handlers/IMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MyUpdatedBot.Core.Handlers
+{
+    public interface IMessageHandler
+    {
+        bool CanHandle(Message? message);
+        Task HandleAsync(ITelegramBotClient botClient, Message message, CancellationToken ct);
+    }
+}
