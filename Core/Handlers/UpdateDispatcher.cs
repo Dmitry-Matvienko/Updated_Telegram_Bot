eae5407888a60c5b5fce8af683417fdbc3257d41
Дима <dima.matvienko239@gmail.com>
2025-12-27 06:53:26 +0200

    feat(dispatcher): handle all user messages

--- Core/Handlers/UpdateDispatcher.cs
+++ Core/Handlers/UpdateDispatcher.cs
@@ -8,59 +8,91 @@ namespace MyUpdatedBot.Core.Handlers
     {
         private readonly IEnumerable<ICommandHandler> _commandHandlers;
         private readonly IEnumerable<IButtonHandlers> _buttonHandlers;
+        private readonly IEnumerable<IMessageHandler> _messageHandlers;
         private readonly ILogger<UpdateDispatcher> _logger;
 
-        public UpdateDispatcher(IEnumerable<ICommandHandler> commandHandlers, ILogger<UpdateDispatcher> logger, IEnumerable<IButtonHandlers> buttonHandlers)
+        public UpdateDispatcher(IEnumerable<ICommandHandler> commandHandlers, ILogger<UpdateDispatcher> logger, 
+                                IEnumerable<IButtonHandlers> buttonHandlers, IEnumerable<IMessageHandler> messageHandlers)
         {
             _commandHandlers = commandHandlers;
             _logger = logger;
             _buttonHandlers = buttonHandlers;
+            _messageHandlers = messageHandlers;
         }
 
         public async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken ct)
         {
-            _logger.LogInformation("[UpdateDispatcher]: update {UpdateId} of type {UpdateType}",
+            _logger.LogDebug("[UpdateDispatcher]: update {UpdateId} of type {UpdateType}",
             update.Id,
             update.Type);
 
-            if (update.Message?.Text is string text)
+            if (update.Message is Message msg)
             {
-                // foreach all the text handlers and looking for the right one
-                foreach (var handler in _commandHandlers)
+                foreach (var messHandler in _messageHandlers)
                 {
-                    if (!handler.CanHandle(text))
-                        continue;
-
-                    _logger.LogInformation("[UpdateDispatcher]: Update {UpdateId}: handler {Handler} will process text \"{Text}\". User: {FristName} - {UserId}",
-                        update.Id,
-                        handler.GetType().Name,
-                        text,
-                        update.Message?.From?.FirstName,
-                        update.Message?.From?.Id);
-
                     try
                     {
-                        _logger.LogDebug("[UpdateDispatcher]: User: {FirstName} sent message: {Text}. UserId: {UserId}, UpdateId: {UpdateId}",
-                                update.Message?.From?.FirstName,
-                                update.Message?.Text,
-                                update.Message?.From?.Id,
-                                update.Id);
+                        if (!messHandler.CanHandle(msg)) continue;
 
-                        await handler.HandleAsync(client, update.Message, ct);
+                        _logger.LogInformation("[UpdateDispatcher]: Update {UpdateId}: handler {Handler} will process message \"{message}\". User: {FristName} - {UserId}",
+                            update.Id,
+                            messHandler.GetType().Name,
+                            msg.Type,
+                            update.Message?.From?.FirstName,
+                            update.Message?.From?.Id);
+
+                        await messHandler.HandleAsync(client, msg, ct);
 
                         _logger.LogInformation("[UpdateDispatcher]: Update {UpdateId}: handler {Handler} completed successfully",
-                            update.Id,
-                            handler.GetType().Name);
+                                update.Id,
+                                messHandler.GetType().Name);
                     }
                     catch (Exception ex)
                     {
-                        _logger.LogError(ex, "[UpdateDispatcher]: Update {UpdateId}: handler {Handler} threw exception",
-                            update.Id,
-                            handler.GetType().Name);
+                        _logger.LogError(ex, "[UpdateDispatcher]: message-handler {Handler} threw for update {UpdateId}",
+                            messHandler.GetType().Name, update.Id);
                     }
                 }
-            }
 
+                if (msg.Text is string text)
+                {
+                    // foreach all the text handlers and looking for the right one
+                    foreach (var handler in _commandHandlers)
+                    {
+                        if (!handler.CanHandle(text))
+                            continue;
+
+                        _logger.LogInformation("[UpdateDispatcher]: Update {UpdateId}: handler {Handler} will process text \"{Text}\". User: {FristName} - {UserId}",
+                            update.Id,
+                            handler.GetType().Name,
+                            text,
+                            update.Message?.From?.FirstName,
+                            update.Message?.From?.Id);
+
+                        try
+                        {
+                            _logger.LogDebug("[UpdateDispatcher]: User: {FirstName} sent message: {Text}. UserId: {UserId}, UpdateId: {UpdateId}",
+                                    update.Message?.From?.FirstName,
+                                    update.Message?.Text,
+                                    update.Message?.From?.Id,
+                                    update.Id);
+
+                            await handler.HandleAsync(client, msg, ct);
+
+                            _logger.LogInformation("[UpdateDispatcher]: Update {UpdateId}: handler {Handler} completed successfully",
+                                update.Id,
+                                handler.GetType().Name);
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, "[UpdateDispatcher]: Update {UpdateId}: handler {Handler} threw exception",
+                                update.Id,
+                                handler.GetType().Name);
+                        }
+                    }
+                }
+
+            }
             // foreach all the inline button handlers and looking for the right one
             if (update.CallbackQuery is CallbackQuery callback)
             {
