6ced239762268064d8d669f698971094593ca4dd
Дима <dima.matvienko239@gmail.com>
2025-12-28 01:38:43 +0200

    feat(message-stats): route all user messages through IMessageHandler
    - CountTextMessageHandler -> CountMessageHandler

---
+++ Core/Handlers/CountMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using MyUpdatedBot.Services.MessageStats;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MyUpdatedBot.Core.Handlers
+{
+    public class CountMessageHandler : IMessageHandler
+    {
+        private readonly IMessageCountStatsService _messageCount;
+
+        public CountMessageHandler(IMessageCountStatsService messageCount)
+        {
+            _messageCount = messageCount;
+        }
+
+        public bool CanHandle(Message? message)
+        {
+            if (message is null) return false;
+            return message.From != null && message.Chat != null;
+        } // Сapture all messages from user
+
+        public Task HandleAsync(ITelegramBotClient botClient, Message message, CancellationToken ct)
+        {
+            if (message.From is null) return Task.CompletedTask;
+
+            _messageCount.EnqueueMessage(message.From!.Id, message.Chat.Id, message.From.FirstName, message.From.Username);
+            return Task.CompletedTask;
+        }
+    }
+}
