d2a08297a8af41a7cc941b2a40c5952221d01932
Дима <dima.matvienko239@gmail.com>
2026-01-03 07:12:04 +0200

    feat(spam): add WarningRecord entity for warning tracking

---
+++ Core/Models/Entities/WarningRecord.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace MyUpdatedBot.Core.Models.Entities
+{
+    public class WarningRecord
+    {
+        public long Id { get; set; }
+        public long ChatId { get; set; }
+        public long UserRefId { get; set; }
+        public UserEntity? User { get; set; }
+        public int WarningsCount { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
