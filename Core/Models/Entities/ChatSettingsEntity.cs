7520f83308d6eda367913364826b7ca22456a3b4
Дима <dima.matvienko239@gmail.com>
2026-01-19 23:42:35 +0200

    feat(settings): add entity for per-chat feature flags

---
+++ Core/Models/Entities/ChatSettingsEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUpdatedBot.Core.Models.Entities
+{
+    public class ChatSettingsEntity
+    {
+        public long Id { get; set; }
+        public long ChatId { get; set; }
+
+        // Feature flags
+        public bool SpamProtectionEnabled { get; set; }
+        public bool LinksAllowed { get; set; }
+    }
+}
