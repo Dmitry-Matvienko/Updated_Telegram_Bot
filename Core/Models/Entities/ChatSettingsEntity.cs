d072df9110f4811dfa36f4524f5799a882c13dec
Дима <dima.matvienko239@gmail.com>
2026-03-11 12:56:42 +0200

    chore(code): remove unused usings and format files

--- Core/Models/Entities/ChatSettingsEntity.cs
+++ Core/Models/Entities/ChatSettingsEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyUpdatedBot.Core.Models.Entities
+﻿namespace MyUpdatedBot.Core.Models.Entities
 {
     public class ChatSettingsEntity
     {
